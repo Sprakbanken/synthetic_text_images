7880fb76b2d7d808e230ab9c8d736e6a85303ac6
Marie Roald <marie.roald@nb.no>
2024-09-14 08:21:54 +0200

    Add XML loading scratchpad

---
+++ scripts/xml-data.py
@@ -0,0 +1,18 @@
+from lxml import etree
+
+# Parse the XML file
+tree = etree.parse("input/SIKOR_sme_20151010/SIKOR_free_sme_20151010.xml")
+root = tree.getroot()
+
+# Count tags
+subcorpus_count = len(root.xpath("//subcorpus"))
+text_count = len(root.xpath("//text"))
+sentence_count = len(root.xpath("//sentence"))
+
+print(f"Subcorpus tags: {subcorpus_count}")
+print(f"Text tags: {text_count}")
+print(f"Sentence tags: {sentence_count}")
+
+sentence_words = {
+    sentence.get("id"): sentence.xpath(".//w/@word") for sentence in root.xpath("//sentence")
+}
